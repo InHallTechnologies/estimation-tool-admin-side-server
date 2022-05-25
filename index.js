eb1f7cbb1aa76849d155df1cdafa868c10c15530
Rishabh Verma <rv.rishabhverma1996@gmail.com>
2022-05-25 18:34:12 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+const staticFolder = express.static(path.join(__dirname, 'public'));
+app.use(staticFolder);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server running at port ${PORT}`)
+})
