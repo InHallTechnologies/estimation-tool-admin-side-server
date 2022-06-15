0cfcaecf9c2b302a8aedaf1a368008aa258ee37b
Rishabh Verma <rv.rishabhverma1996@gmail.com>
2022-06-16 01:12:19 +0530

    bug introduced

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const PORT = process.env.PORT || 3001;
 
 const staticFolder = express.static(path.join(__dirname, 'public'));
 app.use(staticFolder);
-
+asdasdasdnasdas
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 app.listen(PORT, () =>{
     console.log(`Server running at port ${PORT}`)
-})
+});
